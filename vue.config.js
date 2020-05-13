8351a1d0464de07efc4ad72a4b68ef14c775c366
Brendan Ejike <ejike.br@gmail.com>
2020-05-13 20:28:58 +0100

    [modify] config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Locator',
+    themeColor: '#546E7A',
+    msTileColor: '#26A69A',
+    manifestOptions: {
+      background_color: '#819CA9'
+    }
+  }
+}
