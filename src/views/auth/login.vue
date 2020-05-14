92b44d35ef4da4de48933977df103a76647f5404
Brendan Ejike <ejike.br@gmail.com>
2020-05-14 10:15:07 +0100

    Setup authentication

---
+++ src/views/auth/login.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="ui centered grid" style="margin-top: 30px">
+    <div class="column" style="max-width: 450px;">
+      <form action="#" class="ui segment large form">
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui left icon input large">
+              <input type="email" placeholder="Email Address" v-model="form.email" />
+              <i class="ui icon mail"></i>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="ui left icon input large">
+              <input type="password" placeholder="Password" v-model="form.password" />
+              <i class="ui icon lock"></i>
+            </div>
+          </div>
+
+          <button class="ui button field large green" @click.prevent="signIn">Login</button>
+        </div>
+      </form>
+
+      <router-link
+        :to="{name: 'register'}"
+        tag="a"
+        class="ui button basic"
+      >Don't have an account? Register</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    };
+  },
+
+  methods: {
+    signIn() {
+      this.$firebase.auth()
+        .signInWithEmailAndPassword(this.form.email, this.form.password)
+        .then(user => console.log(user))
+        .catch(e => console.log(e));
+    }
+  }
+};
+</script>
---
+++ src/views/auth/register.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="ui centered grid" style="margin-top: 30px">
+    <div class="column" style="max-width: 450px;">
+      <form action="#" class="ui segment large form">
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui left icon input large">
+              <input type="text" placeholder="Name" v-model="form.name" />
+              <i class="ui icon user"></i>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="ui left icon input large">
+              <input type="email" placeholder="Email Address" v-model="form.email" />
+              <i class="ui icon mail"></i>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="ui left icon input large">
+              <input type="password" placeholder="Password" v-model="form.password" />
+              <i class="ui icon lock"></i>
+            </div>
+          </div>
+
+          <button
+            class="ui button field large green"
+            @click.prevent="register"
+            type="submit"
+          >Register</button>
+        </div>
+      </form>
+
+      <router-link :to="{name: 'login'}" tag="a" class="ui button basic">Already registered? Log In</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: null,
+        email: null,
+        password: null
+      }
+    };
+  },
+
+  methods: {
+    register() {
+      this.$firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.form.email, this.form.password)
+        .then(user => {
+          console.log(`Name: ${this.form.name}, Email: ${user.email}, id: ${user.uid}`);
+          this.setUpUserProfile(user);
+        })
+        .catch(e => console.log(e));
+    },
+
+    setUpUserProfile({ user }) {
+      try {
+        this.$firestore.collection("users")
+          .doc(user.uid)
+          .set({
+            name: this.form.name,
+            email: user.email,
+            active: false
+          });
+          console.log("User created Successfully!");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+};
+</script>
