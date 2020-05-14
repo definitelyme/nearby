11ab979d368dbd7668d27007cfb9fe6cf7ae9361
Brendan Ejike <ejike.br@gmail.com>
2020-05-14 10:17:21 +0100

    [Minor] changes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{name: 'home'}">Home</router-link> |
+      <router-link :to="{name: 'about'}">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+*a {
+  color: rgb(25, 147, 247);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
