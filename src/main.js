6238eaeede48ad5a8aad400cb26837d052f0bdcd
Brendan Ejike <ejike.br@gmail.com>
2020-05-14 10:16:50 +0100

    Inject Custom plugin

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import MyPlugin from './foundation/my-plugin';
 
 Vue.config.productionTip = false
+Vue.use(MyPlugin);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
