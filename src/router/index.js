30b8d7809fe9e326ac0d58a2278fda339260edc7
Brendan Ejike <ejike.br@gmail.com>
2020-05-14 10:16:09 +0100

    [add] routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,93 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import firebase from "../foundation/firebase-init";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = [{
+        path: "/",
+        name: "home",
+        component: Home,
+        meta: {
+            requiresAuth: true,
+        },
+    },
+
+    {
+        path: "/admin/u/0",
+        name: "admin",
+        component: () => "../views/admin/index.vue",
+        meta: {
+            requiresAuth: true,
+        },
+    },
+
+    {
+        path: "/register",
+        name: "register",
+        component: () =>
+            import ("../views/auth/register.vue"),
+        meta: {
+            guest: true,
+        },
+    },
+
+    {
+        path: "/login",
+        name: "login",
+        component: () =>
+            import ("../views/auth/login.vue"),
+        meta: {
+            guest: true,
+        },
+    },
+
+    {
+        path: "/about",
+        name: "about",
+        component: () =>
+            import ("../views/About.vue"),
+        meta: {
+            guest: true,
+        },
+    },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+    firebase.auth().onAuthStateChanged((firebaseUser) => {
+        if (!firebaseUser && to.matched.some((record) => record.meta.requiresAuth))
+            next({
+                name: "login",
+            });
+        else if (firebaseUser) {
+            if (to.matched.some((record) => record.meta.guest)) {
+                next(from.fullPath);
+            } else {
+                firebase
+                    .firestore()
+                    .collection("roles")
+                    .doc(firebaseUser.uid)
+                    .get()
+                    .then((snapshot) => {
+                        if (snapshot.data().isAdmin) {
+                            next({ name: "admin" });
+                        } else {
+                            next({ name: "home" });
+                        }
+                    });
+            }
+        } else {
+            next();
+        }
+        next();
+    });
+});
+
+export default router;
