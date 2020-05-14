304f384207cc776db34d89e78732dc4c6f313d70
Brendan Ejike <ejike.br@gmail.com>
2020-05-14 12:20:44 +0100

    [Add] Vuetify dependency

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import MyPlugin from './foundation/my-plugin';
+import './foundation/vuetify';
 
 Vue.config.productionTip = false
 Vue.use(MyPlugin);
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
