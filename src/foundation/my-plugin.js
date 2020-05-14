041a74389b86b27a967de605b5cb356f3036cabb
Brendan Ejike <ejike.br@gmail.com>
2020-05-14 12:21:09 +0100

    [Add] focus directive

--- src/foundation/my-plugin.js
+++ src/foundation/my-plugin.js
@@ -2,5 +2,10 @@ import Vue from 'vue';
 import mixin from '@/foundation/mixin';
 
 export default Plugin.install = () => {
-    Vue.mixin(mixin);
+  Vue.mixin(mixin);
+  Vue.directive('focus', {
+    inserted: (el) => {
+      el.focus();
+    }
+  });
 }
