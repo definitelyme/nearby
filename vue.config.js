bf8bceb1f3f00c54df8309840ba6010677013394
Brendan Ejike <ejike.br@gmail.com>
2020-05-15 17:32:50 +0100

    First deploy

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,22 @@
 module.exports = {
-  "pwa": {
-    "name": "Nearby",
-    "themeColor": "#546E7A",
-    "msTileColor": "#26A69A",
-    "manifestOptions": {
-      "background_color": "#819CA9"
-    }
+  pwa: {
+    name: "Nearby",
+    themeColor: "#546E7A",
+    msTileColor: "#26A69A",
+    manifestOptions: {
+      background_color: "#819CA9",
+      icons: [{
+          src: "/img/logo.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/img/logo.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
   },
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ["vuetify"],
+};
